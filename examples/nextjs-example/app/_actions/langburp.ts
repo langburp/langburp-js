635d043c3d593c4d76e42662d0c71b2f9230b42b
Sasha Varlamov <sasha@sashavarlamov.com>
2025-02-06 23:20:55 -0500

    update langburp example

--- examples/nextjs-example/app/_actions/langburp.ts
+++ examples/nextjs-example/app/_actions/langburp.ts
@@ -1,10 +1,38 @@
 "use server"
 
-import { authorizeEndUser } from "../../utils/langburp-api";
+import { langburpClient } from "../../utils/langburp-client";
 
+// It's important to run this logic server-side, as it requires your secret API key
 export async function authorizeEndUserForLangburp(state?: string) {
   try {
-    return await authorizeEndUser(state);
+    // Instead of dummy data, you can fetch and verify the organization and user from your database here
+    const organization = {
+      id: "example-org-123",
+      name: "Example Organization",
+    };
+    const user = {
+      id: "john-doe-123",
+      name: "John Doe",
+      email: "john.doe@example.com",
+    };
+
+    // Authorize the end user for Langburp and return the authorization response to the Langburp client library
+    return await langburpClient.endUserAuth.authorizeEndUser({
+      authorizeEndUserSchema: {
+        state,
+        connectionMetadata: {
+          id: organization.id,
+          name: organization.name,
+          // You can add any other metadata you want to store for the organization on the "Connection" object
+        },
+        connectionUserMetadata: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          // You can add any other metadata you want to store for the user on the "Connection User" object
+        },
+      },
+    });
   } catch (error) {
     console.error("Error authorizing end user in internal action:", error);
     throw error;
--- examples/nextjs-example/utils/langburp-client.ts
+++ examples/nextjs-example/utils/langburp-client.ts
@@ -9,17 +9,8 @@ if (!process.env.LANGBURP_SECRET_API_KEY) {
   throw new Error('LANGBURP_SECRET_API_KEY is not set');
 }
 
-const langburpClient = new LangburpClient({
+export const langburpClient = new LangburpClient({
   publicApiKey: process.env.NEXT_PUBLIC_LANGBURP_PUBLIC_API_KEY,
   secretApiKey: process.env.LANGBURP_SECRET_API_KEY,
   apiBaseUrl: process.env.NEXT_PUBLIC_LANGBURP_API_BASE_URL,
 })
-
-// TODO User and org data
-export const authorizeEndUser = async (state?: string) => {
-  return await langburpClient.endUserAuth.authorizeEndUser({
-    authorizeEndUserSchema: {
-      state,
-    },
-  });
-};
